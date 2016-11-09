c48c5b04fb98be96f8b161251ad8bd7af56c830c
刘圣洁 <15538509549@163.com>
2016-11-09 14:20:49 +0800

    Initial Commit

---
+++ RongCloudDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RongCloudDemo
+//
+//  Created by 刘圣洁 on 16/11/9.
+//  Copyright © 2016年 LSJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RongCloudDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RongCloudDemo
+//
+//  Created by 刘圣洁 on 16/11/9.
+//  Copyright © 2016年 LSJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
